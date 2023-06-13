014ff0ed16ace3432b72b7f798dea72d9055d50c
jocile <jocile@gmail.com>
2023-06-13 19:40:14 -0300

    Create Aluno.java

---
+++ src/main/java/com/jocile/cadastro6/entidades/Aluno.java
@@ -0,0 +1,69 @@
+package com.jocile.cadastro6.entidades;
+
+import java.util.Date;
+
+class Aluno {
+    private String nome;
+    private char sexo;
+    private int idade;
+    private String matricula;
+    private Date anoDeIngresso;
+
+    public Aluno(String nome, char sexo, int idade, String matricula, Date anoDeIngresso) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.matricula = matricula;
+        this.anoDeIngresso = anoDeIngresso;
+    }
+
+    public Aluno() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getAnoDeIngresso() {
+        return anoDeIngresso;
+    }
+
+    public void setAnoDeIngresso(Date anoDeIngresso) {
+        this.anoDeIngresso = anoDeIngresso;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "nome=" + nome + ", sexo=" + sexo + ", idade=" + idade + ", matricula=" + matricula + ", anoDeIngresso=" + anoDeIngresso + '}';
+    }
+    
+    
+}
